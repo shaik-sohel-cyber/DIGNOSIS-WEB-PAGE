233e65c340d90d0bf37df0c6a895a0ad0bbd06fd
SHAIK SOHEL <sohelshaikb8@gmail.com>
2025-01-21 21:18:31 +0530

    Add files via upload
    It is a specialized web-page for diagnosis service where we can show the results of a patients details and their diagnosis report

---
+++ main.js
@@ -0,0 +1,19 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+        },
+    });
+    win.loadFile('test.html'); // Use your HTML file here
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
+});
